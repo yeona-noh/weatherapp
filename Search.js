fdf932d390842e477c7850e5166089fa9bad0148
yeona-noh <153779215+yeona-noh@users.noreply.github.com>
2023-12-21 16:08:06 -0700

    Update Search.js

--- Search.js
+++ Search.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import axios from "axios";
-const RADAR_API_KEY = "prj_test_pk_bc98832a3852b7f7969e08b3d319fd70e13d2d77"
- 
+const RADAR_API_KEY = "prj_test_pk_bc98832a3852b7f7969e08b3d319fd70e13d2d77";
+const WEATHER_API_KEY = "18a3631b896350dd1d53a177819cffbd";
 const Search = ({onSearch}) => {
     
     const getCoordinate = async (city) => {
@@ -9,30 +9,38 @@ const Search = ({onSearch}) => {
             let res = await axios.get("https://api.radar.io/v1/geocode/forward",{
                 params: { query: city },
                 headers: { Authorization: RADAR_API_KEY }
+
             });
-            return res
+            let lat = res.data.addresses[0].latitude;
+            let lon = res.data.addresses[0].longitude;
+            return {lat,lon}
         }
+
         catch (error) {
             console.log(error)
             return error
         }
         
     };
-        const [location, setLocation] = useState("");
-      
-        const handleSubmit = (event) => {
-            if (location) {
+
+    const [location, setLocation] = useState("");
+    const handleSubmit = async (event) => {
+        if (location) {
                 onSearch(location);
                 setLocation('');
               }
-          event.preventDefault();
-          let res = getCoordinate(location)
-          console.log(res)
-        }
-      
+        event.preventDefault();
+        let {lat,lon} = await getCoordinate(location)
+        // console.log(lat)
+        // console.log(lon)
+
+    }
+    
     return(
         <form onSubmit={handleSubmit}>
-        <input type="text" id="lname" name="lname" value={location} onChange={(e) => setLocation(e.target.value)}/>
+        <input type="text" id="lname" name="lname" 
+        value={location} onChange={(e) => 
+        setLocation(e.target.value)}/>
         <button type="submit">Search</button>
         </form>
     )
