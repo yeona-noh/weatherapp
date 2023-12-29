64633f5f1d45d44645856e45ab660166e8898cc9
yeona-noh <153779215+yeona-noh@users.noreply.github.com>
2023-12-28 19:06:25 -0700

    Update WeatherDisplay.js

--- WeatherDisplay.js
+++ WeatherDisplay.js
@@ -1,10 +1,24 @@
 import React from "react";
-const WeatherDisplay = () => {
-    return(
-      <div>
-        <h1>WeatherDisplay</h1>
-      </div>
-    )
+const WeatherDisplay = ({weatherData}) => {
+    if(weatherData) {
+      return(
+        <div>
+          <h1>WeatherDisplay</h1>
+          <p>Feels like: {weatherData.feels_like}</p>
+          <p>humidity: {weatherData.humidity}</p>
+          <p>pressure: {weatherData.pressure}</p>
+          <p>temp: {weatherData.temp}</p>
+          <p>temp_max: {weatherData.temp_max}</p>
+          <p>temp_min: {weatherData.temp_min}</p>
+        </div>
+      )
+    } else {
+      return (
+        <p>Loading...</p>
+      )
+      
+    }
+
 }
 
 export default WeatherDisplay;
