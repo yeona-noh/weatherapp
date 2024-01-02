2ebdaa10985d65d528c2e196cecd10bd8d868abf
yeona-noh <153779215+yeona-noh@users.noreply.github.com>
2024-01-02 09:41:53 -0700

    Update Search.js

--- Search.js
+++ Search.js
@@ -4,7 +4,7 @@ import WeatherDisplay from "./WeatherDisplay";
 const RADAR_API_KEY = "prj_test_pk_bc98832a3852b7f7969e08b3d319fd70e13d2d77";
 const WEATHER_API_KEY = "18a3631b896350dd1d53a177819cffbd";
 const Search = ({onSearch}) => {
-    const [weatherData, setWeatherData] = useState();
+    const [weatherData, setWeatherData] = useState([]);
 
     const getCoordinate = async (city) => {
         try {
@@ -27,16 +27,17 @@ const Search = ({onSearch}) => {
 
         const getCurrentWeather = async (lat,lon) => {
         try {
-            let response = await axios.get("https://api.openweathermap.org/data/2.5/weather", {
+            let response = await axios.get("https://api.openweathermap.org/data/2.5/forecast", {
                 params: { 
                     lat: lat,
                     lon: lon,
                     appid: WEATHER_API_KEY,
-                    units: "imperial"
+                    units: "imperial",
+                    cnt: 3
                 }
             });
 
-            return response.data.main;
+            return response.data.list;
             
         } catch(error) {
             console.log(error)
@@ -55,20 +56,37 @@ const Search = ({onSearch}) => {
         let main = await getCurrentWeather(lat,lon)
         // console.log(lat)
         // console.log(lon)
-        console.log(main)
+        console.log(main);
         setWeatherData(main);
-
+        
+        
+        
+   
     }
-    
+
     return(
-        <div>
-        <form onSubmit={handleSubmit}>
+        <div className="search">
+        <form onSubmit={handleSubmit} >
         <input type="text" id="lname" name="lname" 
         value={location} onChange={(e) => 
         setLocation(e.target.value)}/>
-        <button type="submit">Search</button>
+        <button className="button" type="submit"></button>
         </form>
-        <WeatherDisplay weatherData={weatherData}/>
+        {weatherData.map(data => {
+            return (
+                <WeatherDisplay 
+                key={data.main.id}
+                time={data.dt_txt}
+                feels={data.main.feels_like}
+                humidity={data.main.humidity}
+                pressure={data.main.pressure}
+                temp={data.main.temp}
+                temp_max={data.main.temp_max}
+                temp_min={data.main.temp_min}
+                />
+            )
+        })}
+        
         </div>
     )
 }
